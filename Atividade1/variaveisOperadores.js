70a98b4279b955407aea7c6c51160492d72a9a55
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-03-13 09:26:25 -0300

    Atividade 01

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,45 @@
+//OPERADORES
+/*Atribuição de adição	x += y	x = x + y
+Atribuição de subtração	x -= y	x = x - y
+Atribuição de multiplicação	x *= y	x = x * y
+Atribuição de divisão	x /= y	x = x / y
+Atribuição de resto	x %= y	x = x % y
+Atribuição exponencial	x **= y	x = x ** y*/
+
+
+// SOMA
+const numASoma = 10;
+const numBSoma = 5;
+const resultadoSoma = numASoma + numBSoma;
+
+//SUBTRAÇÃO
+const numASubtracao = 15;
+const numBSubtracao = 7;
+const resultadoSubtracao = numASubtracao - numBSubtracao;
+
+//MULTIPLICAÇÃO
+const numAMultiplicacao = 8;
+const numBMultiplicacao = 4;
+const resultadoMultiplicacao = numAMultiplicacao * numBMultiplicacao;
+
+//DIVISÃO
+const numADivisao = 20;
+const numBDivisao = 2;
+const resultadoDivisao = numADivisao / numBDivisao;
+
+//RESTO DIVISÃO
+const numARestoDivisao = 11;
+const numBRestoDivisao = 3;
+const resultadoRestoDivisao = numARestoDivisao % numBRestoDivisao;
+
+//EXPONENCIAÇÃO
+const numAExponenciacao = 2;
+const numBExponenciacao = 3;
+const resultadoExponenciacao = numAExponenciacao ** numBExponenciacao;
+
+console.log(`Resultado da soma entre ${numASoma} e ${numBSoma}: ${resultadoSoma}`);
+console.log(`Resultado da subtração entre ${numASubtracao} e ${numBSubtracao}: ${resultadoSubtracao}`);
+console.log(`Resultado da multiplicação entre ${numAMultiplicacao} e ${numBMultiplicacao}: ${resultadoMultiplicacao}`);
+console.log(`Resultado da divisão entre ${numADivisao} e ${numBDivisao}: ${resultadoDivisao}`);
+console.log(`Resultado do resto da divisão entre ${numARestoDivisao} e ${numBRestoDivisao}: ${resultadoRestoDivisao}`);
+console.log(`Resultado da exponenciação de ${numAExponenciacao} por ${numBExponenciacao}: ${resultadoExponenciacao}`);
